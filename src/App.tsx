f8669f13285876edb00e0200f34eb0f4e1ba5789
Ezeko95 <ciangianluca@hotmail.com>
2023-06-15 09:38:11 -0300

    Login Auth0

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,15 @@
-
+import LoginButton from "./components/Login/Login";
+import LogoutButton from "./components/Logout/Logout";
+import Profile from "./components/ProfileUser/ProfileUser";
 
 const App = () => {
   return (
-    <div>AAAAAAAPP</div>
-  )
-}
+    <div>
+      <LoginButton />
+      <LogoutButton />
+      <Profile/>
+    </div>
+  );
+};
 
-export default App
+export default App;
--- src/main.tsx
+++ src/main.tsx
@@ -1,15 +1,23 @@
-import React from 'react'
-import ReactDOM from 'react-dom/client'
-import App from './App.tsx'
-import './index.css'
-import store  from '../src/redux/store/configureStore.ts';
-import { Provider } from 'react-redux';
+import React from "react";
+import ReactDOM from "react-dom/client";
+import App from "./App.tsx";
+import "./index.css";
+import store from "../src/redux/store/configureStore.ts";
+import { Provider } from "react-redux";
+import { Auth0Provider } from "@auth0/auth0-react";
 
-
-ReactDOM.createRoot(document.getElementById('root') as HTMLElement).render(
-  <Provider store={store}>
-    <React.StrictMode>
+ReactDOM.createRoot(document.getElementById("root") as HTMLElement).render(
+  <Auth0Provider
+    domain="walletwise.us.auth0.com"
+    clientId="o4nR12XFZsF5SOiAbWxHhi3bBEU0DiKp"
+    authorizationParams={{
+      redirect_uri: window.location.origin,
+    }}
+  >
+    <Provider store={store}>
+      <React.StrictMode>
         <App />
-    </React.StrictMode>
-  </Provider>,
-)
+      </React.StrictMode>
+    </Provider>
+  </Auth0Provider>
+);
